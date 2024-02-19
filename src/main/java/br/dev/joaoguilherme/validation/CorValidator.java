81c3cb507d77efc9d563808f379d206c5ab8ed41
João Guilherme <jaaumgui@gmail.com>
2024-02-19 11:51:46 -0300

    feat(Cor/CorValidator): Adicionado anotação de validação de cor em String

---
+++ src/main/java/br/dev/joaoguilherme/validation/CorValidator.java
@@ -0,0 +1,20 @@
+package br.dev.joaoguilherme.validation;
+
+import br.dev.joaoguilherme.validation.annotation.Cor;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class CorValidator implements ConstraintValidator<Cor, String> {
+
+    private Cor.TipoCor tipoCor;
+    @Override
+    public void initialize(Cor constraintAnnotation) {
+        this.tipoCor = constraintAnnotation.tipo();
+    }
+
+    @Override
+    public boolean isValid(String cor, ConstraintValidatorContext context) {
+        return cor == null || cor.matches(tipoCor.getRegex());
+    }
+}
---
+++ src/main/java/br/dev/joaoguilherme/validation/annotation/Cor.java
@@ -0,0 +1,57 @@
+package br.dev.joaoguilherme.validation.annotation;
+
+import br.dev.joaoguilherme.validation.CorValidator;
+import jakarta.validation.Constraint;
+
+import br.dev.joaoguilherme.validation.annotation.Cor.List;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Repeatable(List.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = CorValidator.class)
+public @interface Cor {
+
+    String message() default "Cor inválida";
+
+    TipoCor tipo() default TipoCor.HEX;
+
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        Cor[] value();
+    }
+
+    enum TipoCor {
+        HEX("^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$"),
+        RGB("^rgb\\((\\d{1,3}),\\s*(\\d{1,3}),\\s*(\\d{1,3})\\)$"),
+        HSL("^hsl\\((\\d{1,3}),\\s*(\\d{1,3})%,\\s*(\\d{1,3})%\\)$"),
+        HSV("^hsv\\((\\d{1,3}),\\s*(\\d{1,3})%,\\s*(\\d{1,3})%\\)$"),
+        CMYK("^cmyk\\((\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(\\d{1,3})%\\)$"),
+        RGBA("^rgba\\((\\d{1,3}),\\s*(\\d{1,3}),\\s*(\\d{1,3}),\\s*(0|1|0?\\.\\d+)\\)$"),
+        HSLA("^hsla\\((\\d{1,3}),\\s*(\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(0|1|0?\\.\\d+)\\)$"),
+        HSVA("^hsva\\((\\d{1,3}),\\s*(\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(0|1|0?\\.\\d+)\\)$"),
+        CMYKA("^cmyka\\((\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(\\d{1,3})%,\\s*(0|1|0?\\.\\d+)\\)$");
+
+        private final String regex;
+
+        TipoCor(String regex) {
+            this.regex = regex;
+        }
+
+        public String getRegex() {
+            return regex;
+        }
+    }
+}
