295325718f90688256dc429b31c60e909ed219ac
João Guilherme <jaaumgui@gmail.com>
2024-02-19 11:16:02 -0300

    feat(Nome/NomeValidator): Adicionado anotação para validar nomes

---
+++ src/main/java/br/dev/joaoguilherme/validation/NomeValidator.java
@@ -0,0 +1,22 @@
+package br.dev.joaoguilherme.validation;
+
+
+import br.dev.joaoguilherme.validation.annotation.Nome;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NomeValidator implements ConstraintValidator<Nome, String> {
+
+    private String regex;
+
+    @Override
+    public void initialize(Nome constraintAnnotation) {
+        this.regex = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String nome, ConstraintValidatorContext context) {
+        if (nome == null) return false;
+        return nome.matches(regex);
+    }
+}
---
+++ src/main/java/br/dev/joaoguilherme/validation/annotation/Nome.java
@@ -0,0 +1,22 @@
+package br.dev.joaoguilherme.validation.annotation;
+
+import br.dev.joaoguilherme.validation.NomeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = NomeValidator.class)
+public @interface Nome {
+    String message() default "Nome inválido";
+    String regexp() default "^(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+(?:\\-(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+)*(?: (?:(?:e|y|de(?:(?: la| las| lo| los))?|do|dos|da|das|del|van|von|bin|le) )?(?:(?:(?:d'|D'|O'|Mc|Mac|al\\-))?(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+|(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+(?:\\-(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+)*))+(?: (?:Jr\\.|II|III|IV))?$";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
