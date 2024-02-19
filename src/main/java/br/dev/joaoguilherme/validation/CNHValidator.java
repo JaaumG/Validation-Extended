b0d48bd4aa652d599991a869850e8cf3ad322789
João Guilherme <jaaumgui@gmail.com>
2024-02-19 16:02:19 -0300

    feat(CNH/CNHValidator): Adicionado anotação de validação de CNH em String

---
+++ src/main/java/br/dev/joaoguilherme/validation/CNHValidator.java
@@ -0,0 +1,20 @@
+package br.dev.joaoguilherme.validation;
+
+import br.dev.joaoguilherme.validation.annotation.CNH;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class CNHValidator implements ConstraintValidator<CNH, String> {
+
+    private String regex;
+    @Override
+    public void initialize(CNH constraintAnnotation) {
+        regex = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String cnh, ConstraintValidatorContext context) {
+        return cnh == null || cnh.matches(regex);
+    }
+}
---
+++ src/main/java/br/dev/joaoguilherme/validation/annotation/CNH.java
@@ -0,0 +1,32 @@
+package br.dev.joaoguilherme.validation.annotation;
+
+import br.dev.joaoguilherme.validation.CNHValidator;
+import br.dev.joaoguilherme.validation.annotation.CNH.List;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Repeatable(List.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = CNHValidator.class)
+public @interface CNH {
+
+    String message() default "CNH inválido";
+
+    String regexp() default "((cnh.*[0-9]{11})|(CNH.*[0-9]{11})|(habilitação.*[0-9]{11})|(carteira.*[0-9]{11}))";
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        CNH[] value();
+    }
+}
