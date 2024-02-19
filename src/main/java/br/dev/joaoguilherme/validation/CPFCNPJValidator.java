8e744fa960f2acbd69b41b9a8bee439df4afa631
João Guilherme <jaaumgui@gmail.com>
2024-02-19 16:06:16 -0300

    feat(CPF_CNPJ/CPFCNPJValidator): Adicionado anotação de validação de CPF ou CNPJ em String

---
+++ src/main/java/br/dev/joaoguilherme/validation/CPFCNPJValidator.java
@@ -0,0 +1,21 @@
+package br.dev.joaoguilherme.validation;
+
+import br.dev.joaoguilherme.validation.annotation.CEP;
+import br.dev.joaoguilherme.validation.annotation.CPF_CNPJ;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class CPFCNPJValidator implements ConstraintValidator<CPF_CNPJ, String> {
+
+    private String regex;
+    @Override
+    public void initialize(CPF_CNPJ constraintAnnotation) {
+        regex = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String cpfCnpj, ConstraintValidatorContext context) {
+        return cpfCnpj == null || cpfCnpj.matches(regex);
+    }
+}
