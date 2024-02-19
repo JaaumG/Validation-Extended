bdad9d5967c42fb0efee3a85ebe405e69ebc70cb
João Guilherme <jaaumgui@gmail.com>
2024-02-19 16:11:41 -0300

    feat(Boleto/BoletoValidator): Adicionado anotação de validação de Boleto em String

---
+++ src/main/java/br/dev/joaoguilherme/validation/BoletoValidator.java
@@ -0,0 +1,20 @@
+package br.dev.joaoguilherme.validation;
+
+import br.dev.joaoguilherme.validation.annotation.Boleto;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class BoletoValidator implements ConstraintValidator<Boleto, String> {
+
+    private String regex;
+    @Override
+    public void initialize(Boleto constraintAnnotation) {
+        regex = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String boleto, ConstraintValidatorContext context) {
+        return boleto == null || boleto.matches(regex);
+    }
+}
---
+++ src/main/java/br/dev/joaoguilherme/validation/annotation/Boleto.java
@@ -0,0 +1,33 @@
+package br.dev.joaoguilherme.validation.annotation;
+
+import br.dev.joaoguilherme.validation.BoletoValidator;
+import br.dev.joaoguilherme.validation.CEPValidator;
+import br.dev.joaoguilherme.validation.annotation.Boleto.List;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Repeatable(List.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = BoletoValidator.class)
+public @interface Boleto {
+
+    String message() default "Boleto ou Linha Digitável inválidos";
+
+    String regexp() default "(\\d{5}[\\.]\\d{5}[\\s]\\d{5}[\\.]\\d{6}[\\s]\\d{5}[\\.]\\d{6}[\\s]\\d[\\s]\\d{14})|(\\d{47,48})|(\\d{12} \\d{12} \\d{12} \\d{12})";
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        Boleto[] value();
+    }
+}
