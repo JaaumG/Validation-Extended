639032fa01f113d05fd52943a9591b23c6ea1ddb
João Guilherme <jaaumgui@gmail.com>
2024-02-19 11:20:11 -0300

    refactor(Idade): Adicionado anotação interna para utilizar em collections

--- src/main/java/br/dev/joaoguilherme/validation/annotation/Idade.java
+++ src/main/java/br/dev/joaoguilherme/validation/annotation/Idade.java
@@ -4,13 +4,17 @@ import br.dev.joaoguilherme.validation.IdadeValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-@Target({ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
+import br.dev.joaoguilherme.validation.annotation.Idade.List;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Repeatable(List.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
 @Constraint(validatedBy = IdadeValidator.class)
 public @interface Idade {
     String message() default "A idade mínima é de %s anos";
@@ -20,4 +24,12 @@ public @interface Idade {
     Class<? extends Payload>[] payload() default {};
 
     int minAge();
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        Idade[] value();
+    }
 }
